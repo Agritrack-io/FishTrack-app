b9bfb878aed4c71affa17ea07cb42e90d3792640
chris georgiadis <cg@rfid.gr>
2021-07-27 15:14:02 +0300

    add wh menu

---
+++ app/src/main/java/io/agritrack/fishtrack/ui/activity/WhMenuActivity.java
@@ -0,0 +1,16 @@
+package io.agritrack.fishtrack.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.agritrack.fishtrack.R;
+
+public class WhMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wh_menu);
+    }
+}
