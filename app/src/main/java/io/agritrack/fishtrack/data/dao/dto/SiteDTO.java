764b1f4c82e49c183a35086ffa6555d39712b58e
George Skrimpas <georges@agritrack.io>
2021-08-05 16:46:13 +0300

    add DTO

---
+++ app/src/main/java/io/agritrack/fishtrack/data/dao/dto/SiteDTO.java
@@ -0,0 +1,23 @@
+package io.agritrack.fishtrack.data.dao.dto;
+
+import io.agritrack.fishtrack.data.model.Site;
+
+public class SiteDTO {
+    public Long id;
+    public String code;
+    public String name;
+    public String description;
+    public String siteLevel;
+    public Boolean active;
+
+    public static Site convert(SiteDTO siteDTO) {
+        Site site = new Site();
+        site.id = siteDTO.id;
+        site.code = siteDTO.code;
+        site.name = siteDTO.name;
+        site.description = siteDTO.description;
+        site.siteLevel = siteDTO.siteLevel;
+        site.active = siteDTO.active;
+        return site;
+    }
+}
