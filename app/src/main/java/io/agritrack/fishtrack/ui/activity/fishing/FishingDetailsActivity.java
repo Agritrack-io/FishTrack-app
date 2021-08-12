6c5335287ae4912edd8cd731150749443cb57d4f
George Skrimpas <georges@agritrack.io>
2021-08-12 14:17:09 +0300

    fixes

--- app/src/main/java/io/agritrack/fishtrack/ui/activity/fishing/FishingDetailsActivity.java
+++ app/src/main/java/io/agritrack/fishtrack/ui/activity/fishing/FishingDetailsActivity.java
@@ -2,18 +2,26 @@ package io.agritrack.fishtrack.ui.activity.fishing;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 import io.agritrack.fishtrack.R;
+import io.agritrack.fishtrack.data.MobileDB;
+import io.agritrack.fishtrack.data.model.AppUser;
 import io.agritrack.fishtrack.ui.service.LocalPreferences;
 
 import static io.agritrack.fishtrack.FishTrackApplication.getContext;
 
 public class FishingDetailsActivity extends AppCompatActivity {
+    private MobileDB db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,25 @@ public class FishingDetailsActivity extends AppCompatActivity {
         // set Header Info
         TextView tvHeader = findViewById(R.id.tvHeaderFishingDetails);
         tvHeader.setText(LocalPreferences.HeaderMsg());
+
+        // load users with Harvest role and fill in the spHarvest Spinner.
+        List<AppUser> ichthyopathologists = db.userDAO().getByRole("ROLE_PATHOLOGIST");
+        if(ichthyopathologists!=null && !ichthyopathologists.isEmpty()) {
+            String[] harvestRequester = ichthyopathologists.stream().map(x->x.email).toArray(String[]::new);
+            Spinner harvestSpinner = findViewById(R.id.spHarvest);
+            ArrayAdapter<String> hrAdapter = new ArrayAdapter<>(this, R.layout.simple_spinner_item, harvestRequester);
+            hrAdapter.setDropDownViewResource(R.layout.simple_spinner_item);
+            harvestSpinner.setAdapter(hrAdapter);
+        }
+
+
+
+
+
+
+
+
+
 
         configFooter();
     }
