4bd0e3dd8d4255855e7fd24b00fcc5dfb9f2394d
Nikos Papagiannakis <nikosp@agritrack.io>
2022-06-08 21:17:48 +0300

    api

--- app/src/main/java/io/agritrack/api/sync/SyncApi.java
+++ app/src/main/java/io/agritrack/api/sync/SyncApi.java
@@ -43,7 +43,7 @@ public interface SyncApi {
     Call<AssetDTO> getAssetById(@Path("assetId") String assetId, @Header("Authorization") String token);
 
     @Headers("Content-Type: application/json; charset=utf-8")
-    @GET("/asset/{siteId}?level=2")
+    @GET("/asset/{siteId}")
     Call<List<AssetDTO>> getAssetsBySite(@Path("siteId") UUID siteId, @Header("Authorization") String token);
 
     @Headers("Content-Type: application/json; charset=utf-8")
